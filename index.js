87007060aa44a33ddeb946c0a2716693c4d5d4ac
canicemichael <canicemichael@gmail.com>
2022-06-02 22:54:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(process.env.DB)
+        .then(() => console.log("DBConnection Successful!"))
+        .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running");
+})

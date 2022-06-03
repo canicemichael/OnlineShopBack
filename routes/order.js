064a11ec971ede35fd6a4e6e709286f3721e041a
canicemichael <canicemichael@gmail.com>
2022-06-03 09:31:45 +0100

    Add stripe payment method

--- routes/order.js
+++ routes/order.js
@@ -1,5 +1,6 @@
 const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 const Order = require("../models/Order");
+const { json } = require("express/lib/response");
 
 const router = require("express").Router();
 
@@ -55,5 +56,32 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
     }
 })
 
+// GET MONTHLY INCOME
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+    try {
+        const income = await Order.aggregate([
+            { $match: { createdAt: { $gte: previousMonth } } },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                    sales: "$amount",
+                }
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: "$sales" }
+                }
+            }
+        ]);
+        res.status(200).json(income);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 module.exports = router
